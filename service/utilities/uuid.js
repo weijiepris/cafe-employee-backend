f07dbb612bc0155efe8f65f1e355fafefcdd3204
weijiepris <altxmare@hotmail.com>
2023-06-03 03:22:13 +0800

    updated uuid

--- service/utilities/uuid.js
+++ service/utilities/uuid.js
@@ -1,30 +1,20 @@
+const generateUUID = (length) => {
+  let result = "";
+  const characters = "0123456789abcdef";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+};
+
 const generateEmployeeUUID = () => {
-  const generateRandomHex = (length) => {
-    let result = "";
-    const characters = "0123456789abcdef";
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      result += characters.charAt(randomIndex);
-    }
-    return result;
-  };
-  const uuid = `UI${generateRandomHex(7)}`;
+  const uuid = `UI${generateUUID(7)}`;
   return uuid;
 };
 
 const generateCafeUUID = () => {
-  const generateRandomHex = (length) => {
-    let result = "";
-    const characters = "0123456789abcdef";
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      result += characters.charAt(randomIndex);
-    }
-    return result;
-  };
-  const uuid = `${generateRandomHex(8)}-${generateRandomHex(
-    4
-  )}-${generateRandomHex(4)}-${generateRandomHex(4)}-${generateRandomHex(12)}`;
+  const uuid = `${generateUUID(8)}-${generateUUID(4)}-${generateUUID(4)}-${generateUUID(4)}-${generateUUID(12)}`;
   return uuid;
 };
 
